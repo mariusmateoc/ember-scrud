a3b947d1e3634ca88edc36afa63e672abfe4720a
Marius Mateoc <mateoc.marius@gmail.com>
2014-12-15 18:42:19 +0200

    Add our projects resource

---
+++ app/project/model.js
@@ -0,0 +1,30 @@
+import DS from 'ember-data';
+
+var Project = DS.Model.extend({
+  name: DS.attr('string'),
+  projectType: DS.attr('string'),
+  amount: DS.attr('number')
+});
+
+Project.reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      name: 'Website Redesign',
+      projectType: 'Design',
+      amount: 12000
+    },{
+      id: 2,
+      name: 'Backend Development',
+      projectType: 'Development',
+      amount: 23400
+    },{
+      id: 3,
+      name: 'Social Media Presence',
+      projectType: 'Marketing',
+      amount: 7800
+    }
+  ]
+});
+
+export default Project;
