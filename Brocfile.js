cf394ecbed5884868d9de19738d5441d8bbedb78
Marius Mateoc <mateoc.marius@gmail.com>
2015-01-08 15:54:41 +0200

    Update main templates for generated resources
      • Create clients template
      • Create projects template
      • Create employees template
      • Update styles for created template

--- Brocfile.js
+++ Brocfile.js
@@ -4,20 +4,4 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
-app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
-
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
-  destDir: 'fonts'
-});
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
-  destDir: 'fonts'
-});
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
-  destDir: 'fonts'
-});
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
-  destDir: 'fonts'
-});
-
 module.exports = app.toTree();
