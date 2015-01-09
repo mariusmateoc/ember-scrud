861d74d9966d2fafe0b6ba06b696b0b52bd34c9e
Marius Mateoc <mateoc.marius@gmail.com>
2015-01-09 17:02:22 +0200

    Create individual client page

--- app/router.js
+++ app/router.js
@@ -1,14 +1,15 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
-  this.resource('clients', function() { });
-  this.resource('projects', function() { });
-  this.resource('employees', function() { });
+  this.resource("clients", function() {});
+  this.resource("projects", function() {});
+  this.resource("employees", function() {});
+  this.resource('client', { path: 'client/1' }, function() { });
 });
 
 export default Router;
