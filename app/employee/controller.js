7e131aaf3031b1f3ea29571924ed6d52884564c4
Marius Mateoc <mateoc.marius@gmail.com>
2015-01-22 16:34:55 +0200

    Update employee page so you can edit and delete

--- app/employee/controller.js
+++ app/employee/controller.js
@@ -1,4 +1,36 @@
+
+// /app/employee/controller.js
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ObjectController.extend({
+  isEditing: false,
+
+  actions: {
+    editEmployee: function() {
+      this.set('isEditing', true);
+    },
+    saveChanges: function() {
+      var self = this;
+      this.get('model').save().then(function() {
+        console.log('SUCCESS');
+        self.set('isEditing', false);
+      }, function() {
+        console.log('FAIL');
+      });
+    },
+    cancelChanges: function() {
+      this.get('model').rollback();
+      this.set('isEditing', false);
+    },
+    deleteEmployee: function() {
+      var employee = this.get('model');
+      employee.deleteRecord();
+      if (confirm("Are you sure you want to delete this employee?")) {
+        employee.save();
+        this.transitionToRoute('employees');
+      } else {
+        employee.rollback();
+      }
+    }
+  }
 });
