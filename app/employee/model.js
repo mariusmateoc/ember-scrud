2e0a2531afc10e7276b290ffdffead9f5e334d13
Marius Mateoc <mateoc.marius@gmail.com>
2015-01-22 16:46:35 +0200

    Create fullName computed property on employee model

--- app/employee/model.js
+++ app/employee/model.js
@@ -6,7 +6,11 @@ var Employee = DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
-  occupation: DS.attr('string')
+  occupation: DS.attr('string'),
+
+  fullName: function() {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  }.property('firstName', 'lastName')
 });
 
 Employee.reopenClass({
